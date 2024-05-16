68f6e4afdfca7beb64b12bc32ed11d311bf0746e
jeffsonyu <jeffson-yu@sjtu.edu.cn>
2024-05-16 22:26:17 +0800

    Delete some irrelevant files

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch import nn
-
-criterion = nn.CrossEntropyLoss()
-y_pred = torch.tensor([0, 1], dtype=torch.float32)
-y = torch.tensor(0, dtype=torch.long)
-print(criterion(y_pred, y).item())
